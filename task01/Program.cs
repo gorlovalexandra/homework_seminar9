7f9cea34d8185d3fdb93ad6e2ef0447036a88a03
Alexandra Gorlova <gorlova.alexandra2014@yandex.ru>
2022-10-21 17:15:09 +0300

    added task01

---
+++ task01/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 64: Задайте значение N. 
+Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" */
+
+Console.WriteLine("Enter a natural number greater than 1:");
+int N = int.Parse(Console.ReadLine());
+
+void NumericInterval (int number)
+{
+    if (number < 0) Console.Write($"{number} not a natural number.");
+    if (number == 0) return;
+    Console.Write("{0,4}", number);
+    NumericInterval (number - 1);
+}
+
+NumericInterval(N);
